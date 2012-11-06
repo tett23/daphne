83b952a272fc0f4c86f12cc35d73e7caf64fde6c
tett23 <tett23@gmail.com>
2012-11-07 00:03:58 +0900

    プロジェクトの色設定に基づきissueのリストを表示

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -10,4 +10,23 @@ Daphne.helpers do
   def current
     current_account.first
   end
+
+  def color(item)
+    case item
+    when Issue
+      unless item.project.nil?
+        unless item.project.color.nil?
+          return item.project.color.color_code
+        end
+      end
+    when Project
+      unless item.color.nil?
+        return item.color.color_code
+      end
+    else
+      return nil
+    end
+
+    return nil
+  end
 end
