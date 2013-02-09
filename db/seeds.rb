9dcbd6a22528eb6f5a707217e1dbef371c9a5b37
tett23 <tett23@gmail.com>
2013-02-10 00:02:40 +0900

    SQLiteからMySQLにDBMSを変更したのでauto increment値を巻き直すSQLを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 require 'yaml'
 
 Color.all.destroy!
-Color.repository.adapter.execute('update sqlite_sequence set seq=0 where name="colors"')
+Color.repository.adapter.execute('ALTER TABLE colors AUTO_INCREMENT= 1')
 YAML.load_file('./db/colors.yml').each do |color|
   Color.create(color)
 end
