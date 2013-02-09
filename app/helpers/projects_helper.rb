18948ecf9d35002cd2d6cd719db0cd81619c71be
tett23 <tett23@gmail.com>
2013-02-10 03:36:42 +0900

    wikiページにもプロジェクトヘッダを表示できるように＋プロジェクトページかの判定をヘルパに切り分け

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,7 +1,17 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Daphne.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+  def project_nav
+    if !@project.nil?
+      return partial 'parts/project_nav', :locals=>{:project=>@project}
+    end
+    if !@issue.nil? && !@issue.project.nil?
+      return partial 'parts/project_nav', :locals=>{:project=>@issue.project}
+    end
+    if !@wiki.nil? && !@wiki.project.nil?
+      return partial 'parts/project_nav', :locals=>{:project=>@wiki.project}
+    end
+
+    ''
+  end
 end
