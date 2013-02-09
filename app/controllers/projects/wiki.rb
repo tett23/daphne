f18bc68d878045593eefbf6db3bc41f5f99c80d0
tett23 <tett23@gmail.com>
2013-02-10 02:40:18 +0900

    プロジェクトにwikiをひもづけられるようにした

---
+++ app/controllers/projects/wiki.rb
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+Daphne.controllers :wiki, :parent=>:projects do
+  get :index do
+    @wiki = Wiki.project_index(current_account.id, params[:project_id])
+
+    render 'wiki/show'
+  end
+
+  get :show, :with=>:title do
+    @wiki = Wiki.detail(current_account.id, params[:project_id], params[:title])
+
+    render 'wiki/show'
+  end
+
+  get :edit, :with=>:title do
+    @wiki = Wiki.detail(current_account.id, params[:project_id], params[:title])
+
+    render 'wiki/edit'
+  end
+
+  post :update, :with=>:title do
+    @wiki = Wiki.detail(current_account.id, params[:project_id], params[:title])
+
+    if @wiki.update(params[:wiki])
+      flash[:success] = @wiki.title+'を編集しました'
+
+      if @wiki.is_project_index
+        redirect url(:wiki, :index, :project_id=>params[:project_id])
+      else
+        redirect url(:wiki, :show, :project_id=>params[:project_id], :title=>@wiki.title)
+      end
+    else
+      render 'wiki/show'
+    end
+  end
+end
--- models/wiki.rb
+++ models/wiki.rb
@@ -4,7 +4,31 @@ class Wiki
   property :id, Serial
   property :title, String
   property :body, Text
+  property :is_project_index, Boolean
 
-  belongs_to :issue
-  belongs_to :project
+  belongs_to :account
+  belongs_to :issue, :required=>false
+  belongs_to :project, :required=>false
+
+  def self.detail(account_id, project_id, title)
+    first(
+      account_id: account_id,
+      project_id: project_id,
+      title: title
+    )
+  end
+
+  def self.project_index(account_id, project_id)
+    wiki = first_or_create({
+      account_id: account_id,
+      project_id: project_id,
+      is_project_index: true
+    })
+
+    if wiki.title.blank?
+      wiki.update(:title => wiki.project.title)
+    end
+
+    wiki
+  end
 end
