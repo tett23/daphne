95d04433c026085fcfe47046f2d44cae3794f13c
tett23 <tett23@gmail.com>
2013-02-11 06:38:50 +0900

    プロジェクト内のページにいるときは新規issue発行にプロジェクト名を補完する

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,16 +2,23 @@
 
 Daphne.helpers do
   def project_nav
-    if !@project.nil? && !@project.id.nil?
-      return partial 'parts/project_nav', :locals=>{:project=>@project}
-    end
-    if !@issue.nil? && !@issue.project.nil?
-      return partial 'parts/project_nav', :locals=>{:project=>@issue.project}
-    end
-    if !@wiki.nil? && !@wiki.project.nil?
-      return partial 'parts/project_nav', :locals=>{:project=>@wiki.project}
-    end
+    project = get_project()
+    return partial 'parts/project_nav', :locals=>{:project=>project} unless project.nil?
 
     ''
+  end
+
+  def get_project
+    if !@project.nil? && !@project.id.nil?
+      return @project
+    end
+    if !@issue.nil? && !@issue.project.nil?
+      return @issue.project
+    end
+    if !@wiki.nil? && !@wiki.project.nil?
+      return @wiki.project
+    end
+
+    nil
   end
 end
