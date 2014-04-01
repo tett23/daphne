ac8517afb2d9ed2d26add0605333fe2b32270ae8
tett23 <tett23@gmail.com>
2014-04-01 21:52:03 +0900

    Issue.aggrigateも同じように権限を有するほかのひとの切ったissueを見られるようにした

--- app/controllers/root.rb
+++ app/controllers/root.rb
@@ -3,7 +3,7 @@ Daphne.controllers :root do
   get :index, :map=>'/' do
     @issues = Issue.list(nil).page(params[:page] || 1).per(ISSUE_PER_PAGE)
     @issue_close_count = Issue.aggrigate(nil, :close, current_account.id)
-    @issue_new_count = Issue.aggrigate(nil, , :new, current_account.id)
+    @issue_new_count = Issue.aggrigate(nil, :new, current_account.id)
 
     render 'root/index'
   end
